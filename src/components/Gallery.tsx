d5ebaed9a579048eaae4ad812c7f832544ff9186
guzov <georgy.guzov@apploidmedia.by>
2026-06-09 14:33:17 +0800

    1

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -10,7 +10,7 @@ const chengdu = [
   { src: "/ченду-2.jpg",       label: "Chengdu, China", span: "col-span-1 row-span-1" },
   { src: "/панда-3.jpg",       label: "Chengdu, China", span: "col-span-1 row-span-1" },
   { src: "/ченду-5.jpg",       label: "Chengdu, China", span: "col-span-1 row-span-1" },
-  { src: "/ченду-8.jpg",       label: "Chengdu, China", span: "col-span-1 row-span-1" },
+  { src: "/ченду-6.jpg",       label: "Chengdu, China", span: "col-span-1 row-span-1" },
 ];
 
 const chongqing = [
